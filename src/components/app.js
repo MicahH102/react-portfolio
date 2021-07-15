6be0224ddbc65a545a76487456696ac4e5d1ca09
MicahH102 <hoopermicah07@gmail.com>
2021-07-15 09:28:12 -0600

    Started a template

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio-container'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Jordan Hudgens Portfolio</h1>
+        <h1>Micah Hooper's Portfolio</h1>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
+        <PortfolioContainer/>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
